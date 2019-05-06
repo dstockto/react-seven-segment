80272aeae8054cf7fc47a3a4167a69d62ec9d5ce
David Stockton <dave@davidstockton.com>
2019-05-05 18:35:23 -0600

    Remove the ArrayInput and Bloop functions

--- src/Components/ArrayInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ArrayInput(props) {
-  return (
-    <textarea type="text" name="array" onChange={props.changeHandler} className={"array-input"} />
-  )
-}
-
-export default ArrayInput;
--- src/Components/Bloop.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Bloop(props) {
-  return <div className={"bloop"}>{props.value}</div>
-};
