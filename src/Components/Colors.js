c7a8804e04ba484dc9c5f00c6611f78d75020d04
kellechu <github@kellechu.com>
2020-10-31 17:58:20 -0600

    added blue color

--- src/Components/Colors.js
+++ src/Components/Colors.js
@@ -6,9 +6,12 @@ const green = ['#00CC00', '#11DD11', '#33DD33', '#44DD44'];
 
 const purple = ['#BF52FF', '#6E4F80', '#642A85', '#9941CC'];
 
+const blue = ['#0012b8', '#0219ed', '#293eff', '#1f2dab'];
+
 const Colors = {
   red,
   green,
+  blue,
   purple,
   off
 };
